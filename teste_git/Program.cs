adca48910c1cee19e15de128c358165dbd609fdd
Marcel <marcel.fernandes@gmail.com>
2019-03-06 23:16:57 -0300

    Projeto iniciado

---
+++ teste_git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TESTE");
+            Console.ReadLine();  
+        }
+    }
+}
